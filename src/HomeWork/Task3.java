1d3ee9d1eab16c96600904558d4b97c213c0f424
Rita Tukach <rtukach@gmail.com>
2022-01-27 22:51:36 +0300

    Added Task3;

---
+++ src/HomeWork/Task3.java
@@ -0,0 +1,16 @@
+package HomeWork;
+
+import java.util.Arrays;
+
+public class Task3 {
+    public static void main(String[] args) {
+        int[] array = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        for (int i = 0; i < array.length / 2; i++) {
+            int tmp = array[i];
+            array[i] = array[array.length - i - 1];
+            array[array.length - i - 1] = tmp;
+        }
+        System.out.println(Arrays.toString(array));
+    }
+}
+
