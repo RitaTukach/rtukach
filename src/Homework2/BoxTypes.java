fd553f4671e79809f0afc53f4b106d2a6b30376c
Rita Tukach <rtukach@gmail.com>
2022-02-07 16:08:35 +0300

    Added Tasks 6-9

---
+++ src/Homework2/BoxTypes.java
@@ -0,0 +1,41 @@
+package Homework2;
+
+public enum BoxTypes {
+    CUBE("It is a cube"),
+    REGULAR("It is a regular box"),
+    ENVELOPE("It is an envelope"),
+    ;
+    private final String name;
+    private int length;
+    private int width;
+    private int height;
+
+    BoxTypes(String name) {
+        this.name = name;
+
+    }
+    public int getLength() {
+        return length;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public static String identifyBoxType(int length, int width, int height) {
+        if (height == length && width == height && width > 0) {
+            return CUBE.name;
+        }
+        else if (height == 0 && width <= length && width > 0) {
+            return ENVELOPE.name;
+        }
+        else if (height > 0 && width <= length && width > 0) {
+            return REGULAR.name;
+        }
+        return "Box type cannot be defined";
+    }
+}
---
+++ src/Homework2/MainBoxes.java
@@ -0,0 +1,7 @@
+package Homework2;
+
+public class MainBoxes {
+    public static void main(String[] args) {
+        System.out.println(BoxTypes.identifyBoxType(5, 1, 7));
+    }
+}
