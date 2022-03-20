e1128539a0834352273bacc81d02070aae6f3cef
Rita Tukach <rtukach@gmail.com>
2022-03-20 22:22:56 +0300

    Added Homework 4

---
+++ src/Homework4/AutoDealer.java
@@ -0,0 +1,83 @@
+package Homework4;
+
+import Homework4.Cars.Car;
+import Homework4.Cars.SportsCar;
+import Homework4.Factories.AutoFactory;
+import Homework4.Factories.PassengerCarFactory;
+import Homework4.Factories.SportsCarFactory;
+import Homework4.Factories.TruckCarFactory;
+import Homework4.Parameters.*;
+import Homework4.Services.AddCarOptionsService;
+import Homework4.Services.ChangeColorService;
+import Homework4.Services.ChangeWheelsService;
+import Homework4.Services.RemoveCarOptionsService;
+import Homework4.Storages.StorageImpl;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class AutoDealer<T> {
+
+    private ChangeColorService changeColorService;
+    private ChangeWheelsService changeWheelsService;
+    private RemoveCarOptionsService removeCarOptionsService;
+    private AddCarOptionsService addCarOptionsService;
+    private final StorageImpl sportCarsStorage = new StorageImpl();
+    List<SportsCar> carsList = new ArrayList<>(sportCarsStorage.getSportCarsStorageList());;
+    private TruckCarFactory truckCarFactory = new TruckCarFactory(changeColorService, changeWheelsService,
+            removeCarOptionsService, addCarOptionsService);
+    private PassengerCarFactory passengerCarFactory = new PassengerCarFactory(changeColorService, changeWheelsService,
+            removeCarOptionsService, addCarOptionsService);;
+    private SportsCarFactory sportsCarFactory = new SportsCarFactory(changeColorService, changeWheelsService,
+            removeCarOptionsService, addCarOptionsService, sportCarsStorage);
+    private AutoFactory autoFactory;
+    private RacingTypes racingTypes;
+    private PassengerCarTypes passengerCarTypes;
+    private CargoTypes cargoTypes;
+
+    public AutoDealer(TruckCarFactory truckCarFactory, PassengerCarFactory passengerCarFactory, SportsCarFactory sportsCarFactory,
+                      ChangeColorService changeColorService, ChangeWheelsService changeWheelsService,
+                      RemoveCarOptionsService removeCarOptionsService, AddCarOptionsService addCarOptionsService) {
+        this.truckCarFactory = truckCarFactory;
+        this.passengerCarFactory = passengerCarFactory;
+        this.sportsCarFactory = sportsCarFactory;
+        this.changeColorService = changeColorService;
+        this.changeWheelsService = changeWheelsService;
+        this.removeCarOptionsService= removeCarOptionsService;
+        this.addCarOptionsService = addCarOptionsService;
+    }
+
+    public Car orderCar(Colors color, Models model, WheelSize wheelSize, EngineVolumes engineVolume,
+                        HashSet<CarOptions> carOptionsList, T ... ts) {
+        Car car = null;
+        for (CargoTypes type: CargoTypes.values()) {
+            if (ts[0].equals(type)) {
+                car = truckCarFactory.createCar(color, model, wheelSize, engineVolume, carOptionsList, ts);
+            }
+        }
+        for (RacingTypes type: RacingTypes.values()) {
+            if (ts[0].equals(type)) {
+                car = sportsCarFactory.createSportsCar(color, model, wheelSize, engineVolume, carOptionsList, racingTypes,
+                         passengerCarTypes);
+            }
+        }
+            return car;
+    }
+
+    public void changeColor(Car car, Colors color) {
+        changeColorService.doService(car, color);
+    }
+
+    public void changeWheelSize(Car car, WheelSize wheelSize) {
+        changeWheelsService.doService(car, wheelSize);
+    }
+
+    public void addCarOptions(Car car, CarOptions carOption) {
+        addCarOptionsService.doService(car, carOption);
+    }
+
+    public void removeCarOptions(Car car, CarOptions carOption) {
+        removeCarOptionsService.doService(car, carOption);
+    }
+}
---
+++ src/Homework4/Main.java
@@ -0,0 +1,44 @@
+package Homework4;
+
+import Homework4.Cars.SportsCar;
+import Homework4.Factories.PassengerCarFactory;
+import Homework4.Factories.SportsCarFactory;
+import Homework4.Factories.TruckCarFactory;
+import Homework4.Parameters.*;
+import Homework4.Services.AddCarOptionsService;
+import Homework4.Services.ChangeColorService;
+import Homework4.Services.ChangeWheelsService;
+import Homework4.Services.RemoveCarOptionsService;
+import Homework4.Storages.StorageImpl;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ChangeColorService changeColorService = new ChangeColorService();
+        ChangeWheelsService changeWheelsService = new ChangeWheelsService();
+        RemoveCarOptionsService removeCarOptionsService = new RemoveCarOptionsService();
+        AddCarOptionsService addCarOptionsService = new AddCarOptionsService();
+        StorageImpl sportCarsStorage = new StorageImpl();
+        List<SportsCar> carsList = new ArrayList<>(sportCarsStorage.getSportCarsStorageList());
+        StorageImpl<SportsCar> sportsCarStorage = new StorageImpl<>();
+
+        SportsCarFactory sportsCarFactory = new SportsCarFactory(changeColorService, changeWheelsService,
+                removeCarOptionsService,addCarOptionsService, sportCarsStorage);
+        TruckCarFactory truckCarFactory = new TruckCarFactory(changeColorService, changeWheelsService,
+                removeCarOptionsService,addCarOptionsService);
+        PassengerCarFactory passengerCarFactory = new PassengerCarFactory(changeColorService, changeWheelsService,
+                removeCarOptionsService,addCarOptionsService);
+
+        AutoDealer autoDealer = new AutoDealer(truckCarFactory, passengerCarFactory, sportsCarFactory,
+                changeColorService, changeWheelsService, removeCarOptionsService,
+                addCarOptionsService);
+
+       System.out.println(autoDealer.orderCar(Colors.GRAY, Models.MERCEDES, WheelSize.EIGHTEEN, EngineVolumes.RACING,
+                new HashSet<>(Set.of(CarOptions.BLUETOOTH)), RacingTypes.DRIFT, PassengerCarTypes.COUPE));
+    }
+}
